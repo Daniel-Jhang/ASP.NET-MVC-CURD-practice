c2b7a7e9f8b62c84cbba7550060658791970f2f5
user <user@DESKTOP-QVRS3RM>
2022-04-21 23:16:42 +0800

    加入專案檔案。

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using PagedList;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        northwndEntities db = new northwndEntities();
+
+        int pageSize = 15;
+
+        #region 列表
+        public ActionResult Index(int page = 1)
+        {
+            int currentPage = page < 1 ? 1 : page;
+            var products = db.Products.OrderBy(x => x.ProductID).ToList();
+            var result = products.ToPagedList(currentPage, pageSize);
+            return View(result);
+        }
+        #endregion
+
+        #region 新增
+        public ActionResult Create()
+        {
+            var supplierID = db.Suppliers.OrderBy(x => x.SupplierID).Select(x => new SelectListItem
+            {
+                Text = x.SupplierID.ToString(),
+                Value = x.SupplierID.ToString()
+            });
+            Session["SupplierID"] = supplierID;
+
+            var categoryID = db.Categories.OrderBy(x => x.CategoryID).Select(x => new SelectListItem
+            {
+                Text = x.CategoryID.ToString(),
+                Value = x.CategoryID.ToString(),
+            });
+            Session["CategoryID"] = categoryID;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Products products)
+        {
+            ViewBag.Error = "";
+            if (ModelState.IsValid)
+            {
+                var temp = db.Products.Where(x => x.ProductName == products.ProductName).FirstOrDefault();
+                if (temp != null)
+                {
+                    ViewBag.Error = "上架產品重複";
+                    return View(products);
+                }
+                db.Products.Add(products);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Error = "輸入資料未通過驗證";
+            return View(products);
+        }
+        #endregion
+
+        #region 修改
+        public ActionResult Edit(int ProductID)
+        {
+            var productToEdit = db.Products.Where(x=>x.ProductID == ProductID).FirstOrDefault();
+
+            var supplierID = db.Suppliers.OrderBy(x => x.SupplierID).Select(x => new SelectListItem
+            {
+                Text = x.SupplierID.ToString(),
+                Value = x.SupplierID.ToString()
+            });
+            Session["SupplierID"] = supplierID;
+
+            var categoryID = db.Categories.OrderBy(x => x.CategoryID).Select(x => new SelectListItem
+            {
+                Text = x.CategoryID.ToString(),
+                Value = x.CategoryID.ToString(),
+            });
+            Session["CategoryID"] = categoryID;
+
+            return View(productToEdit);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Products products)
+        {
+            if (ModelState.IsValid)
+            {
+                var productToEdit = db.Products.Where(x => x.ProductID == products.ProductID).FirstOrDefault();
+                productToEdit.ProductName = products.ProductName;
+                productToEdit.SupplierID = products.SupplierID;
+                productToEdit.CategoryID = products.CategoryID;
+                productToEdit.QuantityPerUnit = products.QuantityPerUnit;
+                productToEdit.UnitPrice = products.UnitPrice;
+                productToEdit.UnitsInStock = products.UnitsInStock;
+                productToEdit.UnitsOnOrder = products.UnitsOnOrder;
+                productToEdit.ReorderLevel = products.ReorderLevel;
+                productToEdit.Discontinued = products.Discontinued;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(products);
+        }
+        #endregion
+
+        #region 刪除
+        public ActionResult Delete(int ProductID)
+        {
+            var productToDelete = db.Products.Where(x=>x.ProductID == ProductID).FirstOrDefault();
+            db.Products.Remove(productToDelete);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        #endregion
+    }
+}
---
+++ WebApplication1/Controllers/SupplierController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using PagedList;
+
+namespace WebApplication1.Controllers
+{
+    public class SupplierController : Controller
+    {
+        northwndEntities db = new northwndEntities();
+
+        int pageSize = 10;
+
+        #region 列表
+        // GET: Supplier
+        public ActionResult Index(int page = 1)
+        {
+            int currentPage = page < 1 ? 1 : page;
+            var suppliers = db.Suppliers.OrderBy(x => x.SupplierID).ToList();
+            var result = suppliers.ToPagedList(currentPage, pageSize);
+            return View(result);
+        }
+        #endregion
+
+        #region 新增
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Suppliers suppliers)
+        {
+            ViewBag.Error = false;
+            if (ModelState.IsValid)
+            {
+                var temp = db.Suppliers.Where(x => x.CompanyName == suppliers.CompanyName).FirstOrDefault();
+                if (temp != null)
+                {
+                    ViewBag.Error = true;
+                    return View(suppliers);
+                }
+                db.Suppliers.Add(suppliers);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(suppliers);
+        }
+        #endregion
+
+        #region 修改
+        public ActionResult Edit(int SupplierID = 1)
+        {
+            var supplierToEdit = db.Suppliers.Where(x=>x.SupplierID == SupplierID).FirstOrDefault();
+            return View(supplierToEdit);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Suppliers suppliers)
+        {
+            var supplierToEdit = db.Suppliers.Where(x=>x.SupplierID == suppliers.SupplierID).FirstOrDefault();
+            if (ModelState.IsValid)
+            {
+                supplierToEdit.CompanyName = suppliers.CompanyName;
+                supplierToEdit.ContactName = suppliers.ContactName;
+                supplierToEdit.ContactTitle = suppliers.ContactTitle;
+                supplierToEdit.Address = suppliers.Address;
+                supplierToEdit.City = suppliers.City;
+                supplierToEdit.Region = suppliers.Region;
+                supplierToEdit.PostalCode = suppliers.PostalCode;
+                supplierToEdit.Country = suppliers.Country;
+                supplierToEdit.Phone = suppliers.Phone;
+                supplierToEdit.Fax = suppliers.Fax;
+                supplierToEdit.HomePage = suppliers.HomePage;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(supplierToEdit);
+        }
+        #endregion
+
+        #region 刪除
+        public ActionResult Delete(int SupplierID)
+        {
+            var supplierToDelete = db.Suppliers.Where(x => x.SupplierID == SupplierID).FirstOrDefault();
+            db.Suppliers.Remove(supplierToDelete);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        #endregion
+    }
+}
